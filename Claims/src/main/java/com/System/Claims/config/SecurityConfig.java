8bfd1af3afcbace2fa13f07d6210e314729e49d2
Jose Luis Burbano <jose.burbanob@campusucc.edu.co>
2025-10-21 11:21:10 -0500

    url de vercel en security

--- Claims/src/main/java/com/System/Claims/config/SecurityConfig.java
+++ Claims/src/main/java/com/System/Claims/config/SecurityConfig.java
@@ -44,7 +44,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOriginPatterns(List.of("http://localhost:3000"));
+        config.setAllowedOriginPatterns(List.of("http://localhost:3000",
+        "https://claims-system.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setAllowCredentials(true);
